ea1004b7b43a61b0ba1647162860c35209f53917
FBethke <86567665+fbethke@users.noreply.github.com>
2025-07-18 16:21:42 +0200

    Add solution for 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number, number_2) {
+
+    let result = 0;
+
+    if (number < 0 
+        || number_2 < 0 
+        || Number.isNaN(number)
+        || Number.isNaN(number_2)
+        || !Number.isInteger(number)
+        || !Number.isInteger(number_2)) {
+        return 'ERROR';
+    }
+
+    if (number > number_2) {
+        let number_tmp = number_2;
+        number_2 = number;
+        number = number_tmp;
+    }
+
+    for (let i = number; i <= number_2; i++) {
+
+        result = result + i;
+    }
+    
+    return (result);
 
 };
 
