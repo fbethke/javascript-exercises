da2b4684497d49fc666a4608e4b2495e6ab67403
FBethke <86567665+fbethke@users.noreply.github.com>
2025-07-18 16:39:43 +0200

    Add solution for 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    checkYear = year / 4;
+    checkYear2 = year / 100;
+    checkYear3 = year / 400;
+    if (Number.isInteger(checkYear) 
+        && (!Number.isInteger(checkYear2) 
+        || Number.isInteger(checkYear3))) {
+    return true; }
+    else {
+        return false;
+    }
 
 };
 
