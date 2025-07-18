d7bfdb904284255d05727c42596fa1abefd70a09
FBethke <86567665+fbethke@users.noreply.github.com>
2025-07-18 18:23:36 +0200

    Add solution for 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
+const add = function(input_1, input_2) {
+
+  let result = input_1 + input_2;
+  return result;
 	
 };
 
-const subtract = function() {
+const subtract = function(input_1, input_2) {
+	
+  let result = input_1 - input_2;
+  return result;
+};
+
+const sum = function(input) {
+	
+   let result = input.reduce((acc, current) => acc + current, 0);
+   return result;
+
+};
+
+const multiply = function(input) {
+
+    let result = input.reduce((acc, current) => acc * current, 1);
+    return result;
+};
+
+const power = function(input_1, input_2) {
+
+  let result = input_1 ** input_2;
+  return result;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(input) {
+  
+  if (input === 0) {
+    return 1;
+  }
 
-const multiply = function() {
+  let result = 1;
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  for (let i = 2; i <= input; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
