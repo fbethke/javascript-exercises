58b5ee7655bb87c470215c6af1c0ee8060ecdb72
FBethke <86567665+fbethke@users.noreply.github.com>
2025-07-19 15:57:04 +0200

    Add solution for 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+function reverseString(str) {
+
+  const charArray = str.split('');
+  charArray.reverse();
+  const reversedStr = charArray.join('');
+
+  return reversedStr;
+
+}
+
+function removeNonAlphabetic(str) {
+  return str.replace(/[^a-zA-Z0-9]/g, '');
+}
+
+
+let reversedString = reverseString(string);
+console.log(reversedString);
+
+string = removeNonAlphabetic(string);
+string = string.toLowerCase();
+
+reversedString = removeNonAlphabetic(reversedString);
+reversedString = reversedString.toLowerCase();
+
+if (string == reversedString) {
+    return true;
+    } else {
+        return false;
+    }
 };
+palindromes('racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
